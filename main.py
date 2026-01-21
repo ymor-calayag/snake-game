5eb4308949f7db1677436075eb13f08cac0d83d3
ymor-calayag <jeffreyymor.calayag@gmail.com>
2026-01-21 15:15:12 +0800

    refactor code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,29 +8,14 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-segments = []
-
-x_pos = 0
-for _ in range(3):
-    snake_segments = Turtle("square")
-    snake_segments.up()
-    snake_segments.color("white")
-    snake_segments.goto(x=x_pos, y=0)
-    x_pos -= 20
-    segments.append(snake_segments)
+snake = Snake()
 
  # [0, -20, -40]
 game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-    for segment in range(len(segments) - 1, 0, -1):
-        new_x = segments[segment - 1].xcor()
-        new_y = segments[segment - 1].ycor()
-        segments[segment].goto(new_x, new_y)
-    segments[0].fd(10)
-    segments[0].lt(90)
-        
-        
+
+    snake.move()
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.move_distance = 10
+
+    def create_snake(self):
+        x_pos = 0
+        for _ in range(3):
+            snake_segments = Turtle("square")
+            snake_segments.up()
+            snake_segments.color("white")
+            snake_segments.goto(x=x_pos, y=0)
+            x_pos -= 20
+            self.segments.append(snake_segments)
+    
+    def move(self):
+        for segment in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[segment - 1].xcor()
+            new_y = self.segments[segment - 1].ycor()
+            self.segments[segment].goto(new_x, new_y)
+        self.segments[0].fd(self.move_distance)
+        self.segments[0].lt(90)
